cbcad34bbb7dd929f46b0cf6cdb7e33c0d22675b
jouleion <124351760+jouleion@users.noreply.github.com>
2024-03-26 12:29:35 +0100

    Organized start of a big project

---
+++ prepare_data.py
@@ -0,0 +1,7 @@
+"""
+This is used to prepare the microphone data from the J_Wil_son file
+"""
+
+# serial connection
+
+# save to json
---
+++ record_data.py
@@ -0,0 +1,7 @@
+"""
+This is used to record the microphone data from Wilson
+"""
+
+# serial connection
+
+# save to json
