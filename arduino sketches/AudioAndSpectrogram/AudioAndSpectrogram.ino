f11cd2812d4e5748268495bfc0442f025a2d72ed
jouleion <124351760+jouleion@users.noreply.github.com>
2024-04-11 17:23:34 +0200

    data

---
+++ arduino sketches/AudioAndSpectrogram/AudioAndSpectrogram.ino
@@ -0,0 +1,59 @@
+// main.ino
+#include <driver/i2s.h>
+
+// pin def
+#define pin_button1 0
+#define pin_button2 47
+#define pin_led 17
+
+// define buffer sizes
+#define SAMPLE_BUFFER_SIZE 128
+#define SAMPLE_RATE 1000
+#define N_O_SAMPLES 64
+#define AUDIO_BUFFER_SIZE (SAMPLE_BUFFER_SIZE * N_O_SAMPLES)
+
+#include "MicrophoneHandler.h"
+
+MicrophoneHandler micHandler;
+
+void setup() {
+    // start serial communication
+    Serial.begin(115200);
+
+    // blink LED to indicate start
+    pinMode(pin_led, OUTPUT);
+    digitalWrite(pin_led, HIGH);
+    delay(200);
+    digitalWrite(pin_led, LOW);
+    delay(1000);
+    digitalWrite(pin_led, HIGH);
+
+    // button setup
+    pinMode(pin_button1, INPUT_PULLUP);
+}
+
+void loop() {
+    // read microphone data
+    micHandler.readAudio();
+
+    // compute spectrogram for each audio sample that is recorded
+    micHandler.computeSpectrogramRow();
+
+    // check if spectrogram is ready (filled)
+    if (micHandler.isSpectrogramReady()) {
+
+        // signify that the tiny ml is done reading audio
+        digitalWrite(pin_led, LOW);
+
+        // send the audio buffer
+        micHandler.sendAudio();
+        
+        // send the spectrogram
+
+        micHandler.sendSpectrogram();
+        
+        while (1) {
+            delay(100);
+        }
+    }
+}
